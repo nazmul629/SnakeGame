66241f7b98eb24be90fc86ff5923451d3fd0cb37
nazmul629 <nazmulhossain.qnh@gmail.com>
2021-11-24 20:11:20 +0600

    Snakes food and added misic

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+import random
+from pygame import mixer
 pygame.init()
 
 screen_wight = 600
@@ -7,18 +9,38 @@ screen_hight = 600
 screen = pygame.display.set_mode((screen_wight, screen_hight))
 pygame.display.set_caption("Snake")
 
+mixer.music.load('background.wav')
+mixer.music.play(-1)
+
 # Collor 
 bg = (100,123,150)
 body_inner = (50,175,25)
 body_outer = (100,100,200)
 red = (255,0,0)
-
-def draw_screen():
-    screen.fill(bg)
+foodc = (255,255,255)
+blue = (0, 0, 255)
 
 cell_size= 20
 direction = 1
 update_snake = 0
+
+# Food 
+food =[0,0]
+new_food = True
+food_place = [0,0]
+score = 0 
+
+
+font = pygame.font.SysFont(None,40)
+
+def draw_screen():
+    screen.fill(bg)
+
+def draw_score():
+    score_text = "Score : "+str(score)
+    score_img = font.render(score_text,True,blue)
+    screen.blit(score_img,(5,5))
+
 
 
 
@@ -28,10 +50,18 @@ snake_pos.append([int(screen_wight/2),int(screen_hight/2)+cell_size])
 snake_pos.append([int(screen_wight/2),int(screen_hight/2)+cell_size * 2])
 snake_pos.append([int(screen_wight/2),int(screen_hight/2)+cell_size * 3 ])
 
+
+
+
+
+
 run = True
 while run:
 
     draw_screen()
+    draw_score()
+
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
@@ -45,8 +75,47 @@ while run:
                 direction = 2
             if  event.key == pygame.K_LEFT and direction != 2:
                 direction = 4
+    # Food 
+    if new_food ==True:
+        new_food =False
+        food[0] = cell_size * random.randint(0,(screen_wight/cell_size)-3)
+        food[1] = cell_size * random.randint(0,(screen_hight/cell_size)-3)
 
-    if update_snake > 99:
+    # Draw Food
+    pygame.draw.rect(screen,foodc,(food[0],food[1],cell_size,cell_size),0,5)
+
+    if snake_pos[0] == food:
+        new_food = True
+        new_peice = list(snake_pos[-1])
+       
+        
+        if direction ==1:
+            new_peice[1] += cell_size
+
+            explaration_sound = mixer.Sound('laser.wav')
+            explaration_sound.play()
+        
+        if direction ==2:
+             new_peice[0] -= cell_size 
+             explaration_sound = mixer.Sound('laser.wav')
+             explaration_sound.play()
+
+        if direction ==3:
+            new_peice[1]-= cell_size
+            explaration_sound = mixer.Sound('laser.wav')
+            explaration_sound.play()
+        if direction ==4:
+            new_peice[0]+= cell_size
+            explaration_sound = mixer.Sound('laser.wav')
+            explaration_sound.play()
+        snake_pos.append(new_peice)
+        score +=1
+        # print(score)
+
+
+
+
+    if update_snake > 80:
         update_snake = 0
         snake_pos = snake_pos[-1:] + snake_pos[:-1]
         if direction == 1:
